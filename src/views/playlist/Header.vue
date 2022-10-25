6be452c398193e78c500ccba2e0d639b91fc03cc
一鵺靊一 <2020583117@qq.com>
2022-10-25 18:07:50 +0800

    playList页面初期搭建

---
+++ src/views/playlist/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-card>
+        <ul v-for="(item, i) in topCat" :key="i">
+            <h1>{{ item }}</h1>
+            <li v-for="cat in playListCat">{{ cat.name }}</li>
+        </ul>
+    </el-card>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { usePlayListCatList } from '~/api/api'
+
+let topCat = ref({})
+let playListCat = ref([])
+onMounted(async () => {
+    const res = await usePlayListCatList()
+    console.log(res)
+    topCat.value = res.categories
+    playListCat.value = res.sub
+})
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/playlist/Main.vue
+++ src/views/playlist/Main.vue
@@ -1,12 +1,9 @@
 <template>
-    <Header />
-    <Main />
+    <el-card>main</el-card>
 </template>
 
 <script setup>
 import {} from 'vue'
-import Header from './Header.vue'
-import Main from './Main.vue'
 </script>
 
 <style lang="scss" scoped></style>
