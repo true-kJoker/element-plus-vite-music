1e7073b07ed3dbba8961b1b2e502fc9b0e01fb85
一鵺靊一 <2020583117@qq.com>
2022-10-25 16:55:15 +0800

    rank页面初步完成

--- src/store/rank.ts
+++ src/store/rank.ts
@@ -1,17 +1,17 @@
-import { defineStore } from "pinia";
-import { useNumberFormat, useFilterTime } from "~/utils/number";
+import { defineStore } from 'pinia'
 //1.定义容器
 //2.使用容器的state
 //3.修改state
 //4.使用容器的actions
-export const useRankStore = defineStore("rank", {
-  state: () => {
-    return {
-      rank: {},
-    };
-  },
+export const useRankStore = defineStore('rank', {
+    state: () => {
+        return {
+            rank: {},
+            songsList: []
+        }
+    },
 
-  getters: {},
+    getters: {},
 
-  actions: {},
-});
+    actions: {}
+})
--- src/views/rank/Main.vue
+++ src/views/rank/Main.vue
@@ -1,11 +1,45 @@
 <template>
     <el-card>
-        <el-table></el-table>
+        <el-table :data="songsList">
+            <el-table-column prop="name" label="Name" />
+        </el-table>
+        <el-pagination
+            v-model:currentPage="currentPage"
+            v-model:page-size="pageSize"
+            :page-sizes="[10, 20, 50, 100]"
+            :small="small"
+            :disabled="disabled"
+            :background="background"
+            layout="total,sizes, prev, pager, next"
+            :total="rank.trackCount"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+        />
     </el-card>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref } from 'vue'
+import { useRankStore } from '~/store/rank'
+import { storeToRefs } from 'pinia'
+import { usePlayListTrackAll } from '~/api/api'
+
+const store = useRankStore()
+const { rank, songsList } = storeToRefs(store)
+
+const currentPage = ref(1)
+const pageSize = ref(10)
+const small = ref(false)
+const background = ref(false)
+const disabled = ref(false)
+const handleSizeChange = async val => {
+    const res = await usePlayListTrackAll(rank.value.id, val, currentPage.value)
+    store.songsList = res
+}
+const handleCurrentChange = async val => {
+    const res = await usePlayListTrackAll(rank.value.id, pageSize.value, (val - 1) * pageSize.value)
+    store.songsList = res
+}
 </script>
 
 <style lang="scss" scoped></style>
