148a48f9d4eb60fc5f60693e1a134e9c3e3719d4
liwenkai <2020583117@qq.com>
2022-10-26 20:23:41 +0800

    playlist页面顶部标签完成

--- src/views/playlist/Header.vue
+++ src/views/playlist/Header.vue
@@ -1,24 +1,36 @@
 <template>
-    <el-card>
-        <ul v-for="(item, i) in topCat" :key="i">
-            <h1>{{ item }}</h1>
-            <li v-for="cat in playListCat">{{ cat.name }}</li>
-        </ul>
-    </el-card>
+  <el-card class="">
+    <ul v-for="(item, i) in topCat" :key="i" class="flex">
+      <span class="text-xl font-semibold">{{ item }}</span>
+      <li v-for="cat in playListCat[i]">
+        <el-button link class="ml-5">{{ cat.name }}</el-button>
+      </li>
+    </ul>
+  </el-card>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { usePlayListCatList } from '~/api/api'
+import { ref, onMounted } from "vue";
+import { usePlayListCatList } from "~/api/api";
 
-let topCat = ref({})
-let playListCat = ref([])
+let topCat = ref({});
+let playListCat = ref([]);
 onMounted(async () => {
-    const res = await usePlayListCatList()
-    console.log(res)
-    topCat.value = res.categories
-    playListCat.value = res.sub
-})
+  const res = await usePlayListCatList();
+  console.log(res);
+  topCat.value = res.categories;
+  for (let index = 0; index < 5; index++) {
+    let arr = [];
+    res.sub.forEach((item) => {
+      if (item.category == index) {
+        arr.push(item);
+      }
+    });
+    playListCat.value.push(arr);
+    arr = [];
+  }
+  console.log(playListCat.value);
+});
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/playlist/index.vue
+++ src/views/playlist/index.vue
@@ -1,12 +1,14 @@
 <template>
+  <div class="mt-10">
     <Header />
     <Main />
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
-import Header from './Header.vue'
-import Main from './Main.vue'
+import {} from "vue";
+import Header from "./Header.vue";
+import Main from "./Main.vue";
 </script>
 
 <style lang="scss" scoped></style>
