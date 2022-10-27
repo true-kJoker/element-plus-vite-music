12b5c59c7fcfca158659786f7aa5e0a44564146e
一鵺靊一 <2020583117@qq.com>
2022-10-27 17:40:15 +0800

    playlist页面标签选择

--- src/store/playList.ts
+++ src/store/playList.ts
@@ -1,16 +1,19 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 //1.定义容器
 //2.使用容器的state
 //3.修改state
 //4.使用容器的actions
-export const usePlayListStore = defineStore("playList", {
-  state: () => {
-    return {
-      playList: [],
-    };
-  },
+export const usePlayListStore = defineStore('playList', {
+    state: () => {
+        return {
+            limit: 18,
+            offset: 0,
+            cat: '',
+            playList: []
+        }
+    },
 
-  getters: {},
+    getters: {},
 
-  actions: {},
-});
+    actions: {}
+})
--- src/views/playlist/Header.vue
+++ src/views/playlist/Header.vue
@@ -1,40 +1,49 @@
 <template>
-  <el-card class="">
-    <ul v-for="(item, i) in topCat" :key="i" class="flex">
-      <span class="text-xl font-semibold">{{ item }}</span>
-      <li v-for="cat in playListCat[i]">
-        <el-button link class="ml-5">{{ cat.name }}</el-button>
-      </li>
-    </ul>
-  </el-card>
+    <el-card class="">
+        <ul v-for="(item, i) in topCat" :key="i" class="flex">
+            <span class="text-xl font-semibold">{{ item }}</span>
+            <li v-for="cat in playListCat[i]">
+                <el-button link class="ml-5" @click="catClick(cat.name)">{{ cat.name }}</el-button>
+            </li>
+        </ul>
+    </el-card>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import { usePlayListCatList, useTopPlaylist } from "~/api/api";
-import { usePlayListStore } from "~/store/playList";
-import { storeToRefs } from "pinia";
+import { ref, onMounted } from 'vue'
+import { usePlayListCatList, useTopPlaylist } from '~/api/api'
+import { usePlayListStore } from '~/store/playList'
+import { storeToRefs } from 'pinia'
 
-const store = usePlayListStore();
-const { playList } = storeToRefs(store);
-let topCat = ref({});
-let playListCat = ref([]);
+const store = usePlayListStore()
+const { playList } = storeToRefs(store)
+let topCat = ref({})
+let playListCat = ref([])
 onMounted(async () => {
-  const res = await usePlayListCatList();
-  topCat.value = res.categories;
-  for (let index = 0; index < 5; index++) {
-    let arr = [];
-    res.sub.forEach((item) => {
-      if (item.category == index) {
-        arr.push(item);
-      }
-    });
-    playListCat.value.push(arr);
-    arr = [];
-  }
-  const playList = await useTopPlaylist({ limit: 18 });
-  store.playList = playList;
-});
+    const res = await usePlayListCatList()
+    topCat.value = res.categories
+    for (let index = 0; index < 5; index++) {
+        let arr = []
+        res.sub.forEach(item => {
+            if (item.category == index) {
+                arr.push(item)
+            }
+        })
+        playListCat.value.push(arr)
+        arr = []
+    }
+    const playList = await useTopPlaylist({ limit: 18 })
+    store.playList = playList
+})
+const catClick = async cat => {
+    const playList = await useTopPlaylist({
+        limit: store.limit,
+        offset: store.offset,
+        cat: cat
+    })
+    store.cat = cat
+    store.playList = playList
+}
 </script>
 
 <style lang="scss" scoped></style>
